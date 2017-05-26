63c220a752f039d55750b294924e65466ccaeaaf
ji-hoon bae <new0126@naver.com>
2017-05-26 18:36:31 +0900

    20170526-1

--- src/com/controller/MemberAddController.java
+++ src/com/controller/MemberAddController.java
@@ -38,7 +38,7 @@ public class MemberAddController extends HttpServlet {
 		}
 		String dates = year+month_zero+month+day_zero+day;
 		System.out.println("dates: "+dates);
-		MemberDTO dto = new MemberDTO(userid, passwd, nickname, dates, gender);
+		MemberDTO dto = new MemberDTO(userid, passwd, nickname.trim(), dates, gender);
         MemberService service = new MemberService();
         String target = "";
         try {
